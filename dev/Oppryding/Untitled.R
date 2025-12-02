ecd955d9c28c504d9b0854fd8281a6b3fbf14082
Drugis1994 <sigurdtbb@outlook.com>
2025-12-03 00:11:18 +0100

    Remove .Rproj.user and dev folders

--- dev/Oppryding/Untitled.R
+++
@@ -1,8 +0,0 @@
-
-engine_load <- function(path = file.path("C++", "build", "libengine_v4.dylib")) {
-  path <- normalizePath(path, mustWork = TRUE)
-  if (!is.loaded("engine_poll_R")) {
-    dyn.load(path)
-  }
-  invisible(TRUE)
-}
